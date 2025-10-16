988747b0cfa77e30eb71a928041b455a4c1620fa
smallFledging <59705311+smallFledging@users.noreply.github.com>
2025-10-16 01:16:00 +0100

    centered text next to button, added toLowerCase

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
 let taskInput = document.getElementById("taskFormEl");
 
 let submitBtn = document.getElementById("submit");
-let taskArray = ['Code'];
+let taskArray = ['code'];
 let buttonNum = 0;
 
 
 submitBtn.addEventListener("click", () => {
     
-    if(taskInput.value.trim() === "" || taskArray.includes(taskInput.value) ) {}
+    if(taskInput.value.trim() === "" || taskArray.includes(taskInput.value.toLowerCase()) ) {}
     else {
         buttonNum++;
         let newListItem = document.createElement("li");
@@ -20,7 +20,7 @@ submitBtn.addEventListener("click", () => {
         `;
         let list = document.getElementById("taskList");
         list.appendChild(newListItem);
-        taskArray.push(taskInput.value);
+        taskArray.push(taskInput.value.toLowerCase());
         taskInput.value = "";
     }
 
@@ -28,10 +28,13 @@ submitBtn.addEventListener("click", () => {
 
 function deleteListItem(e) {
     if(confirm("Are you sure you want to delete the task?")) {
-        e.parentElement.remove();
-        let deletedValIndex = taskArray.indexOf(e.parentElement.children[0].innerText.trim());
+        let spanText = e.parentElement.children[0].innerText.trim();
+        let inputText = e.parentElement.children[0].lastChild.value;
+        let deleteVal = spanText === "" ? inputText.trim() : spanText.trim();
+        let deletedValIndex = taskArray.indexOf(deleteVal.toLowerCase());
         if (deletedValIndex > -1) { // only splice array when item is found
             taskArray.splice(deletedValIndex, 1);
+            e.parentElement.remove();
         }
     }
 }
